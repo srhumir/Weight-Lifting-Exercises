da751e0d6938bcdb5cd0f3ed0cad5029f04cc482
srhumir <srh.umir@gmail.com>
2016-04-29 13:06:00 +0200

    the prediction is ran

--- codes2.R
+++ codes2.R
@@ -46,7 +46,7 @@ indSensorList <- sapply(c("_belt", "_arm", "_dumbbell", "_forearm"),
                         function(s) grep(s,names(data)))
 #divide to trian and test
 set.seed(1234)
-indtrain <- createDataPartition(data$classe, p = .7, list = F)
+indtrain <- createDataPartition(data$classe, p = .5, list = F)
 training <- data[indtrain,]
 testing <- data[-indtrain,]
 #pca on trainig
@@ -65,12 +65,55 @@ names(trainingpca)[-1] <- sapply(1:(dim(trainingpca)[2]-1),
 #train RF model
 modelTreepca <- train(classe~.,data=trainingpca, method="rpart")
 #predict on test data
-trainingpcalist <- sapply(1:4, function(i) 
-        predict(pcalistmodel[[i]],training[,indSensorList[[i]]]))
-trainingpca <- data.frame(classe=training$classe)
+testingpcalist <- sapply(1:4, function(i) 
+        predict(pcalistmodel[[i]],testing[,indSensorList[[i]]]))
+testingpca <- data.frame(classe=testing$classe)
 
-for (i in 1:length(trainingpcalist)){
-        trainingpca <- cbind(trainingpca, trainingpcalist[[i]])
+for (i in 1:length(testingpcalist)){
+        testingpca <- cbind(testingpca, testingpcalist[[i]])
 }
-names(trainingpca)[-1] <- sapply(1:(dim(trainingpca)[2]-1),
+names(testingpca)[-1] <- sapply(1:(dim(testingpca)[2]-1),
                                  function(i) paste("PC", i, sep=""))
+
+pred <- predict(modelTreepca, testingpca)
+#confusion matrix
+confusionMatrix(pred, testing$classe)
+#the result is awful
+#reduced the data size to run RF
+ImpVars <- row.names(varImp(modelTreepca)$importance)[varImp(modelTreepca)$importance >0]
+trainingRFpca <- trainingpca[,ImpVars]
+modelRFpca <- train(trainingRFpca, training$classe, method = "rf")
+
+#predict on testing
+testingRFpca <- testingpca[, ImpVars]
+predRFpca <- predict(modelRFpca, testingRFpca)
+#confusiom matrix
+confusionMatrix(predRFpca, testing$classe)
+##ok well 92% accuracy but not yet conviencing
+
+#this time do not rum pca. just choosign the most important features
+modelTree <- train(classe~., data=training, method="rpart")
+Impvars2 <- row.names(varImp(modelTree)$importance)[varImp(modelTree)$importance >25]
+trainingRF <- training[,Impvars2]
+modelRF <- train(trainingRF, training$classe, method = "rf")
+#test the model
+testinRF <- testing[,Impvars2]
+predRF <- predict(modelRF, testing)
+confusionMatrix(predRF, testing$classe)
+##better 96% accuracy
+
+#run it on the rough testing
+##remove statistics
+validation <- roughtesting[,-ind]
+#removing first 7 columns
+validation <- validation[, -c(1:7)]
+#converting all data to numeric
+for (i in 1:(dim(validation)[2]-1)){
+        validation[,i] <- as.numeric(validation[,i])
+}
+
+##removing highly coorelated
+validation <- validation[,-highlyCorrelated]
+validation <- validation[,Impvars2]
+predVali <- predict(modelRF, validation)
+data.frame(num=as.factor(1:20),predVali)
